73f7342911c01105d6aa0167bf8415acfcf0a7d2
Kolya <ryt9992@mail.ru>
2023-03-05 20:06:52 +0500

    1.3

---
+++ src/presenter/trip.js
@@ -0,0 +1,25 @@
+import TripList from '../view/trip-list.js';
+import PointView from '../view/point.js';
+import NewPointView from '../view/point-new';
+import PointEditView from '../view/point-edit';
+import SortView from '../view/sort.js';
+import { render, RenderPosition } from '../render.js';
+
+export default class TripEventsPresenter {
+   constructor() {
+      this.component = new TripList();
+   }
+
+   init(container) {
+      this.container = container;
+
+      render(new SortView(), this.container, RenderPosition.BEFOREEND);
+      render(this.component, this.container);
+      render(new PointEditView(), this.component.getElement(), RenderPosition.BEFOREEND);
+      render(new NewPointView(), this.component.getElement(), RenderPosition.BEFOREEND);
+
+      for (let i = 0; i < 3; i++) {
+         render(new PointView(), this.component.getElement(), RenderPosition.BEFOREEND);
+      }
+   }
+}
