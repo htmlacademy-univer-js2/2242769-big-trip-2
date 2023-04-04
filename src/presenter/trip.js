0f530375392ad30aec211f442bea025a41ad1b2d
Kolya <ryt9992@mail.ru>
2023-04-04 18:38:46 +0500

    fgfh

--- src/presenter/trip.js
+++ src/presenter/trip.js
@@ -1,25 +1,27 @@
 import TripList from '../view/trip-list.js';
-import PointView from '../view/point.js';
-import NewPointView from '../view/point-new';
-import PointEditView from '../view/point-edit';
+import PreviewPointView from '../view/point.js';
+import EditingPointView from '../view/point-edit.js';
 import SortView from '../view/sort.js';
-import { render, RenderPosition } from '../render.js';
+import { render } from '../render.js';
 
 export default class TripEventsPresenter {
-   constructor() {
-      this.component = new TripList();
+   constructor(tripContainer) {
+      this.eventsList = new TripList();
+      this.tripContainer = tripContainer;
    }
 
-   init(container) {
-      this.container = container;
+   init(pointsModel) {
+      this.pointsModel = pointsModel;
+      this.boardPoints = [...this.pointsModel.getPoints()];
+      this.destinations = [...this.pointsModel.getDestinations()];
+      this.offers = [...this.pointsModel.getOffers()];
 
-      render(new SortView(), this.container, RenderPosition.BEFOREEND);
-      render(this.component, this.container);
-      render(new PointEditView(), this.component.getElement(), RenderPosition.BEFOREEND);
-      render(new NewPointView(), this.component.getElement(), RenderPosition.BEFOREEND);
+      render(new SortView(), this.tripContainer);
+      render(this.eventsList, this.tripContainer);
+      render(new EditingPointView(this.boardPoints[0], this.destinations, this.offers), this.eventsList.getElement());
 
-      for (let i = 0; i < 3; i++) {
-         render(new PointView(), this.component.getElement(), RenderPosition.BEFOREEND);
+      for (const point of this.boardPoints) {
+         render(new PreviewPointView(point, this.destinations, this.offers), this.eventsList.getElement());
       }
    }
 }
